4cc21ff8c97fb5b82057a0fe7f8e8bd99f579287
TAR UMT <TAR UMT@FOCS_D303C_166>
2025-03-07 10:40:37 +0800

    (Fixes 188961248)

---
+++ src/main/java/com/mycompany/cakeorderd/CakeOrderD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cakeorderd;
+
+/**
+ *
+ * @author TAR UMT
+ */
+public class CakeOrderD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
